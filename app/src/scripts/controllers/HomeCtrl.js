b6c2aea9f7267c4ae63d931e10c488772ed2515a
Alex Abraham <alex@abraham.net>
2014-10-24 00:29:14 -0400

    Added tachanun dates and logic

--- app/src/scripts/controllers/HomeCtrl.js
+++ app/src/scripts/controllers/HomeCtrl.js
@@ -5,6 +5,8 @@ module.exports = function($scope, HomeService) {
     // create a message to display in our view
     console.log('HomeCtrl Loaded');
 
+    var tachanunDates = HomeService.getTachanunDates();
+
     var getInfo = function() 
     {
         console.log('HomeCtrl.getInfo');
@@ -17,8 +19,52 @@ module.exports = function($scope, HomeService) {
 
         HomeService.getHebrewDate(year, month, day).then(function(result) {
             console.log(result);
+
+            $scope.hebrewDate = result.hebrew;
+
+            if (day < 10) {
+                day = '0' + day
+            } 
+
+            if (month < 10) {
+                month = '0' + month
+            } 
+
+            $scope.englishDate = month + '/' + day + '/' + year;
+
+            $scope.tachanun = calculateTachanun(result.hd + " " + result.hm, result.events);
+
+            console.log($scope.tachanun);
         });
     };    
     
     getInfo();
+
+    var calculateTachanun = function(date, events)
+    {
+        // if (date == "30 Sh'vat") {
+        //     if (events["Rosh Chodesh Adar I"]) {
+        //         return "Rosh Chodesh Adar I"
+        //     } else {
+        //         return "Rosh Chodesh Adar"
+        //     }
+        // }
+
+        if (date == "30 Sh'vat") {
+            for (var i = 0; i < events.length; i++) {
+                if (events[i] == "Rosh Chodesh Adar I") {
+                    return "Rosh Chodesh Adar I";
+                }
+            }
+            return "Rosh Chodesh Adar"
+        }
+
+        else {
+            if (date in tachanunDates) {
+                return tachanunDates[date];
+            } else {
+                return null;
+            }
+        }
+    }
 }
--- app/src/scripts/services/HomeService.js
+++ app/src/scripts/services/HomeService.js
@@ -4,17 +4,6 @@ module.exports = function($http, $q, $rootScope) {
 
     console.log("HomeService Loaded");
 
-    var format = function(project) {
-        project.rename = rename;
-        project.remove = remove;
-        project.changeDescription = changeDescription;
-        project.changeJSON = changeJSON;
-
-        project.created_at = formatTime(project.created_at);
-        project.updated_at = formatTime(project.updated_at);
-
-        return project;
-    }
 
     this.getHebrewDate = function(year, month, day)
     {
@@ -31,123 +20,112 @@ module.exports = function($http, $q, $rootScope) {
         return deferred.promise;
     }
 
-    this.getProjects = function() {
-        console.log("DashboardService.getProjects");
-
-        var deferred = $q.defer();
-
-        Objects.getProjects({}, function(response) {
-            for (var i = 0; i < response.data.length; i++) {
-                response.data[i] = format(response.data[i]);
-            };
-
-            deferred.resolve(response.data);
-        }, function(error) {
-            deferred.reject(error);
-        });
-
-        return deferred.promise;
-    }
-
-    this.getEndpoints = function() {
-        console.log("DashboardService.getEndpoints");
-
-        var deferred = $q.defer();
-
-        Objects.getEndpoints({}, function(response) {
-            for (var i = 0; i < response.data.length; i++) {
-                response.data[i] = format(response.data[i]);
-            };
-
-            deferred.resolve(response.data);
-
-        }, function(error) {
-            deferred.reject(error);
-        });
-
-        return deferred.promise;
-    }
-
-    this.getObjects = function() {
-        console.log("DashboardService.getObjects");
-
-        var deferred = $q.defer();
-
-        Objects.getObjects({}, function(response) {
-            for (var i = 0; i < response.data.length; i++) {
-                response.data[i] = format(response.data[i]);
-            };
-
-            deferred.resolve(response.data);
-        }, function(error) {
-            deferred.reject(error);
-        });
-
-        return deferred.promise;
-    }
-
-
-    var rename = function() {
-        console.log("MainService.rename");
-        var object = this;
-        var newname = prompt("What would you like to rename the object to?");
-        Tasks.rename({
-            id: object.id,
-            name: newname
-        }, function() {
-            $rootScope.$emit('afterModification');
-        });
-    };
-
-    var changeDescription = function(newDescription) {
-        console.log("MainService.changeDescription", newDescription);
-        var object = this;
-        Objects.changeDescription({
-            id: object.id,
-            description: newDescription
-        }, function() {
-            $rootScope.$emit('afterModification');
-        });
-    }
-
-    var changeJSON = function(newJSON) {
-        console.log("MainService.changeJSON", newJSON);
-        var object = this;
-        Objects.changeJSON({
-            id: object.id,
-            json: newJSON
-        }, function() {
-            $rootScope.$emit('afterModification');
-        });
-    }
-
-    this.create = function(data) {
-        console.log("MainService.create");
-        var deferred = $q.defer();
-
-        Objects.create(data, function(response) {
-            deferred.resolve(response.data);
-        }, function(error) {
-            deferred.reject(error);
-        });
-
-        return deferred.promise;
-    };
-
-    var remove = function() {
-        console.log("MainService.remove");
-        var object = this;
-        if (confirm("Do you really want to remove this object?")) {
-            Objects.remove({
-                taskId: object.id,
-            }, function() {
-                console.log(object.name + " removed");
-                $rootScope.$emit('afterModification');
-            });
-        } else {
-            console.log("Deletion Cancelled");
+    this.getTachanunDates = function()
+    {
+        return {
+            "1 Tishrei": "Rosh Hashana",
+            "2 Tishrei": "Rosh Hashana",
+            "9 Tishrei": "Erev Yom Kippur",
+            "10 Tishrei": "Yom Kippur",
+            "11 Tishrei": "In Between Yom Kippur & Sukkot",
+            "12 Tishrei": "In Between Yom Kippur & Sukkot",
+            "13 Tishrei": "In Between Yom Kippur & Sukkot",
+            "14 Tishrei": "Erev Sukkot",
+            "15 Tishrei": "Sukkot",
+            "16 Tishrei": "Sukkot",
+            "17 Tishrei": "Sukkot",
+            "18 Tishrei": "Sukkot",
+            "19 Tishrei": "Sukkot",
+            "20 Tishrei": "Sukkot",
+            "21 Tishrei": "Sukkot",
+            "22 Tishrei": "Sukkot",
+            "23 Tishrei": "Sukkot",
+            "24 Tishrei": "End of Tishrei",
+            "25 Tishrei": "End of Tishrei",
+            "26 Tishrei": "End of Tishrei",
+            "27 Tishrei": "End of Tishrei",
+            "28 Tishrei": "End of Tishrei",
+            "29 Tishrei": "End of Tishrei",
+            "30 Tishrei": "Rosh Chodesh Cheshvan",
+            "1 Cheshvan": "Rosh Chodesh Cheshvan",
+            "30 Cheshvan": "Rosh Chodesh Kislev",
+            "1 Kislev": "Rosh Chodesh Kislev",
+            "25 Kislev": "Chanukah",
+            "26 Kislev": "Chanukah",
+            "27 Kislev": "Chanukah",
+            "28 Kislev": "Chanukah",
+            "29 Kislev": "Chanukah",
+            "30 Kislev": "Chanukah & Rosh Chodesh Tevet",
+            "1 Tevet": "Chanukah &  Rosh Chodesh Tevet",
+            "2 Tevet": "Chanukah",
+            "1 Sh'vat": "Rosh Chodesh Sh'vat",
+            "15 Sh'vat": "Rosh Chodesh Sh'vat",
+            "30 Sh'vat": "Rosh Chodesh Adar",
+            "1 Adar I": "Rosh Chodesh Adar I",
+            "1 Adar": "Rosh Chodesh Adar",
+            "14 Adar I": "Purim Katan",
+            "14 Adar": "Purim",
+            "15 Adar I": "Shushan Purim Katan",
+            "15 Adar": "Shushan Purim",
+            "30 Adar I": "Rosh Chodesh Adar II" ,
+            "14 Adar II": "Purim",
+            "15 Adar II": "Shushan Purim",
+            "1 Adar II": "Rosh Chodesh Adar II",
+            "1 Nisan": "Rosh Chodesh Nisan",
+            "2 Nisan": "No Tachanun During Nisan",
+            "3 Nisan": "No Tachanun During Nisan",
+            "4 Nisan": "No Tachanun During Nisan",
+            "5 Nisan": "No Tachanun During Nisan",
+            "6 Nisan": "No Tachanun During Nisan",
+            "7 Nisan": "No Tachanun During Nisan",
+            "8 Nisan": "No Tachanun During Nisan",
+            "9 Nisan": "No Tachanun During Nisan",
+            "10 Nisan": "No Tachanun During Nisan",
+            "11 Nisan": "No Tachanun During Nisan",
+            "12 Nisan": "No Tachanun During Nisan",
+            "13 Nisan": "No Tachanun During Nisan",
+            "14 Nisan": "Erev Pesach",
+            "15 Nisan": "Pesach",
+            "16 Nisan": "Pesach",
+            "17 Nisan": "Pesach",
+            "18 Nisan": "Pesach",
+            "19 Nisan": "Pesach",
+            "20 Nisan": "Pesach",
+            "21 Nisan": "Pesach",
+            "22 Nisan": "Pesach",
+            "23 Nisan": "No Tachanun During Nisan",
+            "24 Nisan": "No Tachanun During Nisan",
+            "25 Nisan": "No Tachanun During Nisan",
+            "26 Nisan": "No Tachanun During Nisan",
+            "27 Nisan": "No Tachanun During Nisan",
+            "28 Nisan": "No Tachanun During Nisan",
+            "29 Nisan": "No Tachanun During Nisan",
+            "30 Nisan": "Rosh Chodesh Iyyar",
+            "1 Iyyar": "Rosh Chodesh Iyyar",
+            "14 Iyyar": "Pesach Sheini",
+            "18 Iyyar": "Lag BaOmer",
+            "1 Sivan": "Rosh Chodesh Sivan",
+            "2 Sivan": "In Between Rosh Chodesh & Shavuot",
+            "3 Sivan": "In Between Rosh Chodesh & Shavuot",
+            "4 Sivan": "In Between Rosh Chodesh & Shavuot",
+            "5 Sivan": "In Between Rosh Chodesh & Shavuot",
+            "6 Sivan": "Shavuot",
+            "7 Sivan": "Shavuot",
+            "8 Sivan": "Post-Shavuot (Bring Korban Chagigah)",
+            "9 Sivan": "Post-Shavuot (Bring Korban Chagigah)",
+            "10 Sivan": "Post-Shavuot (Bring Korban Chagigah)",
+            "11 Sivan": "Post-Shavuot (Bring Korban Chagigah)",
+            "12 Sivan": "Post-Shavuot (Bring Korban Chagigah)",
+            "30 Sivan": "Rosh Chodesh Tamuz",
+            "1 Tamuz": "Rosh Chodesh Tamuz",
+            "1 Av": "Rosh Chodesh Av",
+            "9 Av": "Tisha B'Av",
+            "15 Av": "Tu B'Av",
+            "30 Av": "Rosh Chodesh Elul",
+            "1 Elul": "Rosh Chodesh Elul",
+            "29 Elul": "Erev Rosh Hashana"
         }
-    };
+    }
 
     return this;
 }
